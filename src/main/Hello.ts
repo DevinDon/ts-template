e08e6da09ef397709807b074116a89623a78419e
Devin Don <I.INF@Outlook.com>
2019-05-29 16:42:48 +0800

    perf(main): perf sample code

--- src/main/Hello.ts
+++
@@ -1,9 +0,0 @@
-import { Name } from './@types';
-
-export function hello(name: Name): string {
-  return `Hello, ${name}!`;
-}
-
-export async function asyncHello(name: Name): Promise<string> {
-  return `Hello, ${name}!`;
-}
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,11 @@
-export * from './hello';
+import { Name } from './@types';
+
+export function hello(name: Name): string {
+  return `Hello, ${name}!`;
+}
+
+export async function asyncHello(name: Name): Promise<string> {
+  return `Hello, ${name}!`;
+}
+
+export * from './@types';
