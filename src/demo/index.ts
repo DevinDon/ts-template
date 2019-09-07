8d02a55b1d0e2b50c42a5f779955635e1014b673
Devin Don <I.INF@Outlook.com>
2019-09-07 21:26:23 +0800

    perf(demo): update demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,9 +1,3 @@
 import { hello } from '../main';
-import { logger } from '@iinfinity/logger';
 
-logger.info(hello('TypeScript template'));
-
-logger.debug('debug');
-logger.error('error');
-logger.info('info');
-logger.warn('warn');
+console.log(hello('TypeScript template'));
