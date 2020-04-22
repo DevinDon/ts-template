8717c6470ab69c4a5be167390282f8c6267732bf
Devin Don <I.INF@Outlook.com>
2020-04-22 21:14:11 +0800

    chore(webpack): add webpack dev

---
+++ webpack.dev.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+function _externals() {
+  let manifest = require('./package.json');
+  let dependencies = manifest.dependencies;
+  let externals = {};
+  for (let p in dependencies) {
+    externals[p] = 'commonjs ' + p;
+  }
+  return externals;
+}
+
+const externals = _externals();
+
+module.exports = {
+  entry: './src/main/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  node: {
+    console: true,
+    global: true,
+    process: true,
+    Buffer: true,
+    __filename: true,
+    __dirname: true,
+    setImmediate: true
+  },
+  target: 'node',
+  externals,
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin()
+  // ]
+};
